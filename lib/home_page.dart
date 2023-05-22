e9984f1f9ee872f73ae6b011c337bf66fb0bf107
vijayinyoutube <vijaythevirtue@gmail.com>
2023-05-22 13:05:37 +0530

    refactor

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -8,22 +10,47 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Isolates')),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const CircularProgressIndicator(),
-          const SizedBox(height: 50),
-          ElevatedButton(
-            child: const Text('Run Heavy Task'),
-            onPressed: () => runHeavyTask(4000000000),
-          ),
-        ],
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircularProgressIndicator(),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              child: const Text('Run Heavy Task'),
+              onPressed: () => useIsolate(),
+              //runHeavyTaskWithOutIsolate(4000000000),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-runHeavyTask(int count) {
+useIsolate() async {
+  final ReceivePort receivePort = ReceivePort();
+  try {
+    await Isolate.spawn(
+        runHeavyTaskIWithIsolate, [receivePort.sendPort, 4000000000]);
+  } on Object {
+    debugPrint('Isolate Failed');
+    receivePort.close();
+  }
+  final response = await receivePort.first;
+
+  print('Result: $response');
+}
+
+int runHeavyTaskIWithIsolate(List<dynamic> args) {
+  SendPort resultPort = args[0];
+  int value = 0;
+  for (var i = 0; i < args[1]; i++) {
+    value += i;
+  }
+  Isolate.exit(resultPort, value);
+}
+
+int runHeavyTaskWithOutIsolate(int count) {
   int value = 0;
   for (var i = 0; i < count; i++) {
     value += i;
